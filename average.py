2c7f49e2c60ead78e8c16242e1b0f34b851ebb1e
Ryan Bignell <ryan07bignell@gmail.com>
2020-06-12 09:53:19 +0930

    initial commit of average script

---
+++ average.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def mean(array):
+    count=0
+    sum = 0
+    for x in array:
+        sum = sum + x
+        count = count + 1
+    return float(sum)/float(count)
+def main():
+    data = np.array([1.0, 2.0, 3.0, 4.0])
+    ave = mean(data)
+    print(data, 'ave:',ave)
+
+if __name__ == '__main__':
+    main()
