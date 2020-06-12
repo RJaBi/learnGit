7f8486b0199f02a492c34476673b153465688ad5
Ryan Bignell <ryan07bignell@gmail.com>
2020-06-12 13:22:28 +0930

    added std.dev calc

--- average.py
+++ average.py
@@ -1,5 +1,4 @@
 import numpy as np
-
 def mean(array):
     count=0
     sum = 0
@@ -10,7 +9,7 @@ def mean(array):
 def main():
     data = np.array([1.0, 2.0, 3.0, 4.0])
     ave = mean(data)
-    print(data, 'ave:',ave)
-
+    stdDev = np.std(data)
+    print(data, 'ave:',ave, 'std.dev:',stdDev)
 if __name__ == '__main__':
     main()
